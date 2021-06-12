5ddc76a15a491e936f133d988cdc5ad2569ec8f1
Tianyi <thua0026@student.monash.edu>
2021-06-12 15:31:02 +1000

    add timestamps

--- src/models/course.js
+++ src/models/course.js
@@ -1,22 +1,27 @@
-const {model, Schema} = require('mongoose');
+const { model, Schema } = require('mongoose');
 
-const schema = new Schema({
-    _id: {
-        type: String,
-        uppercase: true,
-    },
-    name: {
-        type: String,
-        require: true,
-    },
-    description: {
-        type: String,
-        default: 'This is a description',
-    }
-});
+const schema = new Schema(
+	{
+		_id: {
+			type: String,
+			uppercase: true,
+		},
+		name: {
+			type: String,
+			require: true,
+		},
+		description: {
+			type: String,
+			default: 'This is a description',
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
 
 schema.virtual('code').get(function () {
-    return this._id;
+	return this._id;
 });
 
 module.exports = model('Course', schema);
